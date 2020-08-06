48fa9afcabad375d5bb9744aa9dbc9d6c34ab214
darckwind <f.bunout01@ufromail.cl>
2020-08-06 16:42:33 -0400

    se termmina la comunicacion entre cliente y servidor mediante slice y se agrega la interfas grafica para el moonitoreo como el control de la ignicion.

--- server.py
+++ server.py
@@ -1,25 +1,88 @@
-import sys, Ice, time
-import Demo
+import sys, Ice, time, Demo
+from threading import Thread, Event, Lock
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 
 
 class PrinterI(Demo.Printer):
-    def printString(self, s, current=None):
-        if(s == "conectando"):
-            return "coneccion establecida"
+    action = False
+    def __init__(self):
+        super().__init__()
+        self.data=""
 
-        elif(s == "usuario -> quit"):
+    def set_action(self,comand):
+        PrinterI.action = comand
+        print(self.action)
 
-            return "bey bye"
+
+    def printString(self, s,current=None):
+        self.data = s
+        print(self.action)
+        Display.updateLCD(self.data)
+        time.sleep(0.5)
+        if self.action:
+            return "2"
         else:
-            print(s)
-            respuesta = str(input("-->"))
-            time.sleep(0.3)
-            return (respuesta)
+            return "1"
+
+    @staticmethod
+    def connector():
+        with Ice.initialize(sys.argv) as communicator:
+            adapter = communicator.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 12000")
+            object = PrinterI()
+            adapter.add(object, communicator.stringToIdentity("SimplePrinter"))
+            adapter.activate()
+            communicator.waitForShutdown()
+
+class Display(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 200)
+        self.setWindowTitle("Monitor")
+        self.button = QPushButton("Launch")
+        self.lable_temp_cam = QLabel("Temperatura Camara")
+        self.lable_hume_ham = QLabel("Humedad Hambiente")
+        self.lable_temp_ham = QLabel("Temperatura Hambiente")
+        self.button = QPushButton("Launch")
+        self.button_stop = QPushButton("Stop")
+
+        layout = QGridLayout()
+        layout.setSpacing(10)
+        layout.addWidget(self.lable_temp_ham,1,0)
+        layout.addWidget(self.lable_hume_ham,1,1)
+        layout.addWidget(self.lable_temp_cam,1,2)
+        layout.addWidget(lcd_temp_hamb,2,0)
+        layout.addWidget(lcd_humedad,2,1)
+        layout.addWidget(lcd_temp_camara,2,2)
+        layout.addWidget(self.button,3,1)
+        layout.addWidget(self.button_stop,3,2)
+        self.button.clicked.connect(self.launch)
+        self.button_stop.clicked.connect(self.stop_launch)
+        self.setLayout(layout)
+
+    def updateLCD(event):
+        if event != "" and len(str(event).split("@"))==3:
+            lcd_humedad.display(str(event).split("@")[0])
+            lcd_temp_hamb.display(str(event).split("@")[1])
+            lcd_temp_camara.display(str(event).split("@")[2])
+
+    def launch(self):
+        PrinterI().set_action(True)
+
+    def stop_launch(self):
+        PrinterI().set_action(False)
 
 
-with Ice.initialize(sys.argv) as communicator:
-    adapter = communicator.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000")
-    object = PrinterI()
-    adapter.add(object, communicator.stringToIdentity("SimplePrinter"))
-    adapter.activate()
-    communicator.waitForShutdown()
+if __name__ == '__main__':
+    tc = Thread(target=PrinterI.connector)
+    tc.start()
+
+    app = QApplication(sys.argv)
+    lcd_humedad = QLCDNumber()
+    lcd_temp_camara = QLCDNumber()
+    lcd_temp_hamb = QLCDNumber()
+
+    demo = Display()
+    demo.show()
+    sys.exit(app.exec_())
--- treadth.py
+++ treadth.py
@@ -1,46 +1,33 @@
 import _thread, time, sys, serial, Ice, Demo
 
+from threading import Thread, Event, Lock
+
 global_ser = serial.Serial(port='COM7', baudrate=9600, timeout=0, write_timeout=0)
 
-def data_reciver():
-
-   with Ice.initialize(sys.argv) as communicator:
-      base = communicator.stringToProxy("SimplePrinter:default -p 10000")
-      printer = Demo.PrinterPrx.checkedCast(base)
-      if not printer:
-         raise RuntimeError("Invalid proxy")
-      while True:
-         printer.printString("2")
-         global_ser.write(str("s").encode())
-
-   #global_ser.write(input().encode())
-
 def data_sender():
+
    while True:
       response = global_ser.readline()
       if response is not None:
 
          with Ice.initialize(sys.argv) as communicator:
-            base = communicator.stringToProxy("SimplePrinter:default -p 10000")
+            base = communicator.stringToProxy("SimplePrinter:default -p 12000")
             printer = Demo.PrinterPrx.checkedCast(base)
 
             if not printer:
                raise RuntimeError("Invalid proxy")
 
             #envio de los datos de sensoires
-            mensaje = "conectado"
             #manda los valorores de los sensores por mediop de comunicacion serial con un foramto on@12@34
             printer.printString(str(response)[2:][:-5])
-            time.sleep(1)
             #recive el dato y lo pasa a la placa por convercion a byte para la comunicacion serial
-            global_ser.write(str(printer.printString(mensaje)).encode())
+            global_ser.write(str(printer.printString("")).encode())
 
 def main():
    try:
       #se crean hilos para paralelizar el trabajo
-      #_thread.start_new_thread(data_reciver())
-      _thread.start_new_thread(data_sender())
-
+      t = Thread(target=data_sender())
+      t.start()
    except:
       print("Error: unable to start thread")
    while 1:
